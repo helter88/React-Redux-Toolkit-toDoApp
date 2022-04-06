9bd32dd3023324d06eefbf50693babc40845dfaa
Artur <helter@wp.pl>
2022-04-06 12:07:37 +0200

    add input button and handle

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import { useState } from 'react';
+import './App.css';
+
 function App() {
-  return <div className="App">Starting</div>;
+	const [activity, setActivity] = useState('');
+
+	const inputHandler = (e) => {
+		setActivity(e.target.value);
+	};
+
+	const activityHandler = () => {
+		console.log(activity);
+	};
+
+	return (
+		<div className="App">
+			<h1>Your Activity List:</h1>
+			<input
+				type="text"
+				placeholder="Enter your activity"
+				value={activity}
+				onChange={inputHandler}
+			/>
+			<button onClick={activityHandler}>Add actity</button>
+		</div>
+	);
 }
 
 export default App;
